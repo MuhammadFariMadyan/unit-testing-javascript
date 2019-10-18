4bb599a071c40c0b49438d160d9cff23fea56939
Muhammad Fari <fari.msenju@gmail.com>
2019-10-18 11:04:56 +0700

    Create demo-jest ```expect toEqual```

--- demo-jest/demo.test.js
+++ demo-jest/demo.test.js
@@ -4,4 +4,24 @@ describe("Demo test with jest", () => {
        expect(true).toBe(true);
        expect({}).not.toBe({});
     });
+
+    test("testing toEqual", () => {
+        const subject = { name: "Testing-toEqual Expect Jest"};
+        const actual = {
+            name: "Testing-toEqual Expect Jest"
+        };
+        expect(subject).toEqual(actual);
+    });
+
+    const listOfCharacterOnePiece = [
+        {name: "Monkey D.Luffy"},
+        {name: "Roronoa Zorro"},
+        {name: "Portgas D.Ace"}
+    ];
+    const actualCharacter = [
+        {name: "Monkey D.Luffy"},
+        {name: "Roronoa Zorro"},
+        {name: "Portgas D.Ace"}
+    ];
+    expect(listOfCharacterOnePiece).toEqual(actualCharacter);
 });
